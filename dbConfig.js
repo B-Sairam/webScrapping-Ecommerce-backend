537f9d69eb38bd9561d029e904cfefba874b70a7
SAIRAM <87373136+B-Sairam@users.noreply.github.com>
2022-02-16 18:00:23 +0530

    Modified-error

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
-const dbName = 'hackathon'
+const dbName = 'webscrapping'
 const dbURL = `mongodb+srv://sairam77:08052002sai@sairam.pvjby.mongodb.net/${dbName}`;
 
 module.exports={dbURL,mongodb,MongoClient,dbName}
