523ab70644b8f56d0260d0cf349b122a6ad6fe99
SAIRAM <87373136+B-Sairam@users.noreply.github.com>
2022-09-30 15:26:09 +0530

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const dbName = 'webscrapping'
-const dbURL = `mongodb+srv://sairam77:08052002sai@sairam.pvjby.mongodb.net/${dbName}`;
+const dbURL = `mongodb+srv://sairam77:Sairam2002@sairam.pvjby.mongodb.net/${dbName}`;
 
 module.exports={dbURL,mongodb,MongoClient,dbName}
